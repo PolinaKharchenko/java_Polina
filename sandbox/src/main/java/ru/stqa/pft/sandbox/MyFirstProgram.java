0e70e76fc94ae48478987042b4bb461dcab080d6
PolinaKharchenko <havray@list.ru>
2023-02-13 22:48:59 +0700

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("Polina");
 
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " см и " + b + "см = " + area(a, b) + "см");
+
+  }
+
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
