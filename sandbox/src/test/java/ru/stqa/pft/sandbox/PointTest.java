5d00defc0ddcc5560d3f315f6bc812dd03240a7b
PolinaKharchenko <havray@list.ru>
2023-02-16 17:54:30 +0700

    Тесты разделены на два

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -9,8 +9,13 @@ public class PointTest {
   @Test
   public void testdistanceToAnotherPoint(){
     Point P1 = new Point(9,9);
-    Point P2 = new Point(6,6);
     Assert.assertEquals( P.distanceToAnotherPoint(P1),8.48528137423857);
+
+  }
+  @Test
+  public void testdistanceToAnotherPoint1(){
+    Point P2 = new Point(6,6);
     Assert.assertEquals( P.distanceToAnotherPoint(P2),4.242640687119285);
+
   }
 }
