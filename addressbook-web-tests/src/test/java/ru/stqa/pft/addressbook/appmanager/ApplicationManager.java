a83119bba05347c5de8c400d58d8fe3b9cee55e5
PolinaKharchenko <havray@list.ru>
2023-02-28 22:34:18 +0700

    добавлен класс Equality

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -23,12 +24,12 @@ public class ApplicationManager {
     this.browser = browser;
   }
   public void init() {
-    if (browser == BrowserType.FIREFOX){
+    if (Objects.equals(browser, BrowserType.FIREFOX)){
     wd = new FirefoxDriver();}
-    else if (browser == BrowserType.CHROME) {
+    else if (Objects.equals(browser, BrowserType.CHROME)) {
       wd = new ChromeDriver();
     }
-    else if (browser == BrowserType.IE){
+    else if ( Objects.equals(browser, BrowserType.IE)){
       wd = new InternetExplorerDriver();
     }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
