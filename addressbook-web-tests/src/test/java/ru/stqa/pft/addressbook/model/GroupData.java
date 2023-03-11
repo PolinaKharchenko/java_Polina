55457259d9758e720184a040db97ce41cb312dc8
PolinaKharchenko <havray@list.ru>
2023-03-11 23:11:09 +0700

    Реализована сортировка списка групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -6,11 +6,13 @@ public class GroupData {
     private int id;
     private final String name;
     private final String header;
+
+
     private final String footer;
 
     public GroupData (String name, String header, String footer)
     {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -47,16 +49,17 @@ public class GroupData {
                 ", name='" + name + '\'' +
                 '}';
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return id == groupData.id && Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name);
     }
+
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(name);
     }
+
 }
