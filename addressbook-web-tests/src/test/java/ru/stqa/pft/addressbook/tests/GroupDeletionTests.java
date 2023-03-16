f2b180d56ffd97ba9c682d7fa2d10dfb58488a87
PolinaKharchenko <havray@list.ru>
2023-03-16 23:08:31 +0700

    внесены корректировки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -25,11 +25,11 @@ public class GroupDeletionTests extends TestBase {
        List<GroupData> after = app.getGroupHelper().getGroupList();
        Assert.assertEquals(after.size(), before.size() - 1);
 
-      // before.remove(before.size() - 1);
        before.remove(before.size() - 1);
-       Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-       before.sort(byId);
-       after.sort(byId);
+
+      // Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+      // before.sort(byId);
+      // after.sort(byId);
        Assert.assertEquals(before, after);
        //Assert.assertEquals(before, after);
        //Assert.assertEquals(after.size(), before.size() + 1);
