0eff8173763872e2fc4e1775704846ecae9472b0
PolinaKharchenko <havray@list.ru>
2023-04-02 19:46:15 +0700

    Реализовано протоколирование лог-файлов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -19,6 +21,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+
 
     @DataProvider
     public Iterator<Object[]> validGroups() throws IOException {
@@ -59,6 +63,7 @@ public class GroupCreationTests extends TestBase {
     }
     @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
+
         app.goTo().groupPage();
         Groups before = app.group().all();
         app.group().create(group);
@@ -66,7 +71,8 @@ public class GroupCreationTests extends TestBase {
         Groups after = app.group().all();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-        }
+
+    }
 
     @Test
     public void testBadGroupCreation() throws Exception {
